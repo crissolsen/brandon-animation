e2805f1d949f947036279ab465df22d4cff367f1
Christopher <chris@homecode.co.za>
2020-11-10 15:20:11 +0200

    All routes solve, just form, really

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,46 +3,52 @@ export default {
   ssr: false,
 
   // Target (https://go.nuxtjs.dev/config-target)
-  target: 'static',
+  target: "static",
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'barwise-animation',
+    title: "Brandon Barwise | Animation Portfolio",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "Brandon Barwise is a 3D animator who creates models and scenes. He lives in Belgium and does animation freelancing."
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href:
+          "https://res.cloudinary.com/dvwsiufhl/image/upload/v1605014354/BrandonAnimation/favicon_tb0uwn.ico"
+      }
     ]
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios"
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {},
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  }
-}
+  build: {}
+};
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,10 +1,130 @@
 <template>
-    <div>
-        <TheNav />
-        <form>
-            <input type="name">
-            <input type="email">
-            <textarea placeholder="How can I help you?"> </textarea>
-        </form>
+<div>
+    <TheNav />
+  <div id="form-wrapper" v-if= "!submitted">
+    <h1 class="form-title">
+      Enquiry Form
+    </h1>
+    <div class="content">
+      <form name="contact"  @submit.stop.prevent= "submit" netlify>     
+        <label class="form-label" for="name">
+          Name:
+        </label>
+        <input class="form-field" name="name" id="name" v-model= "name"/>
+        <label class="form-label" for="email">
+          Email:
+        </label>
+        <input class="form-field" name="_replyto" id="email" v-model= "email" />
+        <label class="form-label" for="message">
+          How can we help?
+        </label>
+        <textarea class="form-field" name="message" id="message" v-model= "message"></textarea>
+        <button class="form-button" type="submit" value="send" >Send Message</button>
+      </form>
     </div>
+  </div>
+  <div v-else id="submitted">
+    <h1>Thank you for filling out that form. We look forward to chatting with you further.</h1>
+
+    <MailChimpSignUp />
+    <SocialMediaLogos /></div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+      email: '',
+      message: '',
+      submitted: false
+    }
+  },
+//   methods: {
+//     async submit() {
+//       try {
+//         const res = await this.$axios.request({
+//           url: "https://formspree.io/f/mdopzqpo",
+//           method: 'post',
+//           data: this.$data
+//         })
+//         this.submitted = true
+//       } catch (err) {
+//         console.log(err)
+//       }
+//     }
+//   }
+}
+</script>
+
+<style scoped>
+    #form-wrapper {
+      width: 80%;
+      margin: 0.5em auto;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      padding: 1em;
+      font-family: 'Exo', sans-serif;
+    }
+    h1 {
+      text-align: center;
+      color: black;
+      margin-bottom: 0.5em;
+      font-family: 'Exo', sans-serif;
+    }
+    #form-wrapper label {
+      display: block;
+      font-size: 1.2em;
+      line-height: 1.4em;
+      margin-bottom: 0.5em;
+      margin-top: 0.5em;
+    }
+    .content {
+      display: flex;
+      justify-content: center;
+      box-shadow: 1em 1em 2em gray;
+      padding: 0.5em;
+      border-radius: 2em;
+      padding-bottom: 2em;
+    }
+    .content form {
+      width: 80%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+    }
+
+    .content input {
+      height: 2em;
+      border: 0.2em solid gray;
+      border-radius: 0.5em;
+    }
+
+    .content textarea {
+      height: 10em;
+      border: 0.2em solid gray;
+      border-radius: 0.5em;
+      margin-bottom: 1em;
+    }
+
+    .form-button {
+      font-family: inherit;
+      height: 1.5em;
+      background: rgb(0,207,255,0.6);
+      color: black;
+      font-size: 1.5em;
+      cursor: pointer;
+      font-family: 'Exo', sans-serif;
+    }
+
+    .hpot {
+      display: none;
+    }
+
+    #submitted {
+      text-align: center;
+    }
+</style>
+
