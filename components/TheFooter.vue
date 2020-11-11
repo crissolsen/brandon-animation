15373d40c8e014cebf7e7d239e10fc6dddd3ffed
Christopher <chris@homecode.co.za>
2020-11-11 13:42:09 +0200

    Solve last a tag

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <footer>
         <p>&#169; Brandon Barwise 2020</p>
-        <p>Site by <a href="https://homecode.co.za" rel="norefferer" target="_blank">HomeCode</a></p>
+        <p>Site by <a href="https://homecode.co.za" rel="noreferrer" target="_blank">HomeCode</a></p>
     </footer>
 </template>
 
