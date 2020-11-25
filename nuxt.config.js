1b65b18b1e90c8c5304899cb333b6c90970cb710
Christopher <chris@homecode.co.za>
2020-11-25 15:38:38 +0200

    Add html lang attribute

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,6 +7,9 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
+    htmlAttrs: {
+      lang: "en"
+    },
     title: "Brandon Barwise | Animation Portfolio",
     meta: [
       { charset: "utf-8" },
