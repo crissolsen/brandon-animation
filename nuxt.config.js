87d29c47fc563ef4a521fd38ba2f0994304405b5
Christopher Olsen <olsen.criss@gmail.com>
2021-12-28 12:13:03 +0200

    Add lazy load, new showreel

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,8 @@ export default {
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/axios
-    "nuxt-cookie-control"
+    "nuxt-cookie-control",
+    'nuxt-lazy-load'
   ],
 
   cookies: {
--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
         <div id="animations-container">
         </div>
         <video id="showcase-reel" controls>
-          <source src= "https://res.cloudinary.com/bbarwise/video/upload/v1638124772/showreel_b7szkh.mp4">
+          <source src= "https://res.cloudinary.com/bbarwise/video/upload/v1640640585/Showreel_General_hq86yr.mp4">
         </video>
     </main>
     <AboutMe />
